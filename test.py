e7619e3e3a9afef1219c66bbef98e5ccf50aedbf
Raki Rahman <mdrrahman@microsoft.com>
2021-09-16 11:43:50 -0400

    Updated typo in test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-external_ip = "40.88.53.141"
+external_ip = "52.188.143.206"
 
 # Sync
 print("####################### SYNC #######################")
 
-endpoint = 'http://'+ external_ip +':5000/vision/v3.2/read/syncAnalyze?language=en&readingOrder=natural'
+endpoint = 'http://'+ external_ip +':5000/vision/v3.2/read/syncAnalyze'
 
 file = 'https://raw.githubusercontent.com/mdrakiburrahman/form-recognizer/main/artifacts/mortgage.pdf'
 response = requests.post(endpoint, \
@@ -20,7 +20,7 @@ print(json.dumps(json.loads(response.content), indent=4, sort_keys=True))
 # Async
 print("####################### ASYNC #######################")
 
-endpoint = 'http://'+ external_ip +':5000/vision/v3.2/read/Analyze?language=en&readingOrder=natural'
+endpoint = 'http://'+ external_ip +':5000/vision/v3.2/read/analyze'
 
 response = requests.post(endpoint, headers={"accept": "application/json", 
                         "Content-Type" : "application/json"},  
